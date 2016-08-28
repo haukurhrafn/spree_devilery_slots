acb09a84fad8bba1d74043c4eefeea6c4aa74cae
Haukur Thorsteinsson <haukur@haukur.net>
2016-08-28 13:31:27 +0000

    Adjustments for spree 3.0

--- app/overrides/admin.rb
+++ app/overrides/admin.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
+Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_configuration",
                      :name => "add_delivery_resources_to_admin",
                      :insert_bottom => '[data-hook="admin_configurations_sidebar_menu"]',
                      :partial => "spree_delivery_slots/admin/configuration_sidebar_links")
