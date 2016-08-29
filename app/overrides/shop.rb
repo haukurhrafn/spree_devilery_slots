b6666a978d30a85edd1566591d2565fe4192c910
Haukur Thorsteinsson <haukur@haukur.net>
2016-08-29 20:01:56 +0000

    UI

--- app/overrides/shop.rb
+++ app/overrides/shop.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/checkout/_delivery",
                      :name => "add_delivery_date_to_delivery",
-                     :insert_after => "[data-hook='shipping_method_inner']",
+                     :insert_bottom => "[data-hook='shipping_method_inner']",
                      :partial => "spree/checkout/delivery_date",
                      :disabled => false)
 
